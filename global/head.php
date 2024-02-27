1ed66e0b209188636459c373e0d22cfe69df0d38
denzu-go <denzelgo17@gmail.com>
2024-02-27 11:08:31 +0800

    okays

--- global/head.php
+++ global/head.php
@@ -1,11 +1,36 @@
 <head>
-  <title>Nitro &mdash; Website Template by Colorlib</title>
+  <?php
+  $sql = "SELECT * FROM constants_home WHERE title = 'name logo'";
+  $result = $conn->query($sql);
+  while ($fetched = $result->fetch_assoc()) {
+    $show = $fetched['text'];
+
+    echo '
+    <title>'.$show.' &mdash; Portfolio</title>
+    ';
+  }
+  ?>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
   <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <?php
+  $sql = "SELECT * FROM constants_about WHERE title = 'picture'";
+  $result = $conn->query($sql);
+  while ($fetched = $result->fetch_assoc()) {
+    $show = $fetched['text'];
+
+    echo '
+    <link rel="shortcut icon" href="' . $show . '">
+    ';
+  }
+  ?>
+
+
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/jquery-ui.css">
